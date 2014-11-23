3926f6f89428f8af88b164837df7c61091de0bd2
kmiscia <kmiscia@gmail.com>
2014-11-23 15:12:04 -0500

    Add admin breadcrumbs methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def readable_controller(controller)
+    controller = controller.sub(/^admin\//,'')
+    "> ".html_safe << controller.capitalize!
+  end
+
+  def readable_action(action)
+    return "" if action == 'index'
+    "> ".html_safe << action.capitalize
+  end
+  
 end
