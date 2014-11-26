74dc3a98419d6a8aaf797170288c06c04e412824
kmiscia <kmiscia@gmail.com>
2014-11-25 19:16:02 -0500

    remove stray debugger

--- app/controllers/admin/users/registrations_controller.rb
+++ app/controllers/admin/users/registrations_controller.rb
@@ -3,7 +3,6 @@ class Admin::Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    byebug
     params.require(:admin_user).permit(:username, :email, :password, :password_confirmation)
   end
 end
