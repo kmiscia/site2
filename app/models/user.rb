0babdaac003d974552aa03e5b5704630692f00aa
kmiscia <kmiscia@gmail.com>
2014-11-25 19:16:10 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
 
-  has_many :articles
+  has_many :articles, :foreign_key => "posted_by"
+
+  validates_length_of :username, :within => 3..40
+  validates_length_of :password, :within => 5..40
+  validates_presence_of :username, :email, :encrypted_password
+  validates_uniqueness_of :username, :email
+  validates_confirmation_of :password
+  validates_format_of :email, :with => /@/
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
