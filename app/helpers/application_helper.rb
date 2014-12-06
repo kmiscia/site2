31c36026a8b14ba776d2966fba11063750945406
kmiscia <kmiscia@gmail.com>
2014-12-06 16:29:22 -0500

    test application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
   def time_format(time)
-    time.localtime.strftime("%a %b %d at %I:%M%p")
+    time.localtime.strftime("%a %b %d at %I:%M %p").gsub(/\s+/, ' ')
   end
 
   def time_format_admin(time)
-    time.localtime.strftime("%B %e, %G %l:%M %p")
+    time.localtime.strftime("%B %e, %G %l:%M %p").gsub(/\s+/, ' ')
   end
 
-  def side_nav_class(controller)
-    controller_name == controller ? 'active' : 'inactive'
+  def side_nav_class(name)
+    controller_name == name ? 'active' : 'inactive'
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+  helper ApplicationHelper
+
+  it "formats time for admin" do
+    assert_equal time_format_admin(Time.new(2014, 3, 14, 15, 23, 45)), "March 14, 2014 3:23 PM"
+  end
+
+  it "formats time for regular" do
+    assert_equal time_format(Time.new(2014, 3, 14, 15, 23, 45)), "Fri Mar 14 at 03:23 PM"
+  end
+
+  it "returns active when on controller" do
+    assert_equal side_nav_class('test'), "active"
+  end
+
+  it "returns inactive when not on controller" do
+    assert_equal side_nav_class('not test'), "inactive"
+  end
+end
