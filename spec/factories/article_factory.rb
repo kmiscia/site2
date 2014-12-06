416eb5c4e1d61de8a814376f8d36b3a5a72dae79
kmiscia <kmiscia@gmail.com>
2014-12-06 15:50:06 -0500

    article tests

--- spec/factories/article_factory.rb
+++ spec/factories/article_factory.rb
@@ -1,14 +1,16 @@
+include ActionDispatch::TestProcess
+
 # This will guess the User class
 FactoryGirl.define do
+
   factory :article do
-
     association :user
-
     title "Article Title"
     header_one "Article Header One"
     header_two "Article Header Two"
     url "http://www.testurl.com"
     description "This is a test article!"
-    body "This is the article body. This is the article body. This is the article body. This is the article body."
+    body "This is the article body."
+    photo { fixture_file_upload(Rails.root.join('spec/fixtures/test_image.png'), 'image/png') }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,4 +7,71 @@ describe Article do
   end
 
   it { should have_many(:comments) }
+  it { should belong_to(:user) }
+  it { should belong_to(:category) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:url) }
+
+  it "is not cropping with no cropping attributes" do
+    refute @article.cropping?
+  end
+
+  it "is not cropping with some cropping attributes" do
+    @article.crop_x = 100
+    refute @article.cropping?
+  end
+
+  it "is cropping when cropping attributes are present" do
+    @article.crop_x = 100
+    @article.crop_y = 100
+    @article.crop_w = 100
+    @article.crop_h = 100
+    assert @article.cropping?
+  end
+
+  it "does not process photo during save when not cropping" do
+    expect(@article.photo).to_not receive(:reprocess!)
+
+    @article.save_and_process(@article.attributes)
+    @article.save_and_process(@article.attributes.merge({
+      crop_x: 100,
+      crop_y: 100
+    }))
+  end
+
+  it "does process photo during save when not valid" do
+    expect(@article.photo).to_not receive(:reprocess!)
+
+    @article.save_and_process(@article.attributes.merge({
+      title: "",
+      crop_x: 100,
+      crop_y: 100,
+      crop_h: 100,
+      crop_w: 100
+    }))
+  end
+
+  it "does process photo during save when not cropping" do
+    expect(@article.photo).to receive(:reprocess!)
+
+    @article.save_and_process(@article.attributes.merge({
+      crop_x: 100,
+      crop_y: 100,
+      crop_h: 100,
+      crop_w: 100
+    }))
+  end
+
+  it "updates attributes during valid save" do
+    assert @article.save_and_process({ title: "new title"})
+    assert_equal @article.title, "new title"
+  end
+
+  it "does not update attributes during invalid save" do
+    refute @article.save_and_process({ title: ""})
+    refute_equal @article.title, "new title"
+  end
+
 end
