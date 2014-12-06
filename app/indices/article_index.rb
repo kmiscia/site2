cefca498fce32c21ec8889d57d195cd3eb4f248d
kmiscia <kmiscia@gmail.com>
2014-12-06 15:49:40 -0500

    add thinking sphinx index for article

---
+++ app/indices/article_index.rb
@@ -0,0 +1,18 @@
+ThinkingSphinx::Index.define :article, :with => :active_record do
+  indexes title
+  indexes header_one
+  indexes header_two
+  indexes url
+  indexes description
+  indexes body
+  indexes user.username, :as => :user
+  indexes user.email, :as => :user_email
+  indexes category.name, :as => :category
+  indexes comment.body, :as => :comment
+  indexes author.name, :as => :author
+  has category_id, :as => :category_id
+  has posted_at
+  has created_at
+  where 'posted_at IS NOT NULL'
+  set_property :delta => true
+end
