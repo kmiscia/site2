bca8e6a00489f5cded0ea4f4d76fc1856fcd7c39
kmiscia <kmiscia@gmail.com>
2014-12-07 17:20:46 -0500

    safe regex in annotation

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -3,7 +3,7 @@ class Annotation < ActiveRecord::Base
   belongs_to :gallery_image
 
   validates_format_of :gps_coordinates,
-    with: /^[ ]*[-]?[0-9]+.[0-9]+[ ]*,[ ]*[-]?[0-9]+.[0-9]+[ ]*/,
+    with: /\A[ ]*[-]?[0-9]+.[0-9]+[ ]*,[ ]*[-]?[0-9]+.[0-9]+[ ]*/,
     allow_blank: true,
     message: "is in invalid format."
 end
