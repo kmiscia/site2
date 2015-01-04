f1ea51b7596aca9deb4925b668454f2e9bbe5e18
kmiscia <kmiscia@gmail.com>
2015-01-04 11:31:58 -0500

    remove front end fields from article index for now

--- app/indices/article_index.rb
+++ app/indices/article_index.rb
@@ -8,8 +8,8 @@ ThinkingSphinx::Index.define :article, :with => :active_record do
   indexes user.username, :as => :user
   indexes user.email, :as => :user_email
   indexes category.name, :as => :category
-  indexes comment.body, :as => :comment
-  indexes author.name, :as => :author
+  #indexes comment.body, :as => :comment
+  #indexes author.name, :as => :author
   has category_id, :as => :category_id
   has posted_at
   has created_at
