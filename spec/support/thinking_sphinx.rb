cefc621062852ccf19de8810ee70754d95350059
kmiscia <kmiscia@gmail.com>
2015-03-09 21:27:54 -0400

    break up spec and rails helpers

---
+++ spec/support/thinking_sphinx.rb
@@ -0,0 +1,24 @@
+module SphinxHelpers
+  def index
+    ThinkingSphinx::Test.index
+    sleep 0.25 until index_finished?
+  end
+
+  def index_finished?
+    Dir[Rails.root.join(ThinkingSphinx::Test.config.indices_location, '*.{new,tmp}*')].empty?
+  end
+end
+
+RSpec.configure do |config|
+
+  config.include SphinxHelpers
+
+  config.before(:suite) do  
+    ThinkingSphinx::Test.init(false)
+    ThinkingSphinx::Test.start_with_autostop
+  end
+
+  config.before(:each) do |example|
+    index
+  end
+end
