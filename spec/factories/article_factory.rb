80d228d0a9d6df78c0027d00ccebff34937c0ea0
kmiscia <kmiscia@gmail.com>
2015-03-09 21:28:08 -0400

    Factory articles as with a posted_at time

--- spec/factories/article_factory.rb
+++ spec/factories/article_factory.rb
@@ -11,6 +11,7 @@ FactoryGirl.define do
     url "http://www.testurl.com"
     description "This is a test article!"
     body "This is the article body."
+    posted_at Time.now - 5
     photo { fixture_file_upload(Rails.root.join('spec/fixtures/test_image.png'), 'image/png') }
   end
 end
