e628ba3217cec55c73dd02ec5822e142c0fa43fb
kmiscia <kmiscia@gmail.com>
2015-03-12 19:13:14 -0400

    remove stray character

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-s
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
