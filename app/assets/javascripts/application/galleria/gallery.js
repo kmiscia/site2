6a87d085958a735b64ec500f80a594fd1432e59a
kmiscia <kmiscia@gmail.com>
2015-08-02 13:30:28 -0400

    reference galleria plugin with absolute path

--- app/assets/javascripts/application/galleria/gallery.js
+++ app/assets/javascripts/application/galleria/gallery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   if($(".gallery_container").length){
-    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+    Galleria.loadTheme('/galleria/themes/classic/galleria.classic.min.js');
     Galleria.run('.gallery_container');
   }
 });
