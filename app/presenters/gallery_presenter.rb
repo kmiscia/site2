189b168b63118b07ad6a71487765ecb2176924e0
kmiscia <kmiscia@gmail.com>
2015-08-02 11:59:48 -0400

    randomize gallery images

--- app/presenters/gallery_presenter.rb
+++ app/presenters/gallery_presenter.rb
@@ -3,7 +3,7 @@ require 'delegate'
 class GalleryPresenter < SimpleDelegator
   
   def gallery_images
-    __getobj__.gallery_images.map do |gallery_image|
+    __getobj__.gallery_images.shuffle.map do |gallery_image|
       GalleryImagePresenter.new(gallery_image)
     end
   end
