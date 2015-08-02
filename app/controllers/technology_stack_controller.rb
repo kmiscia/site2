db3c65e6de0db561720bc8dbdbc1a48301893902
kmiscia <kmiscia@gmail.com>
2015-08-02 13:29:33 -0400

    not using tech stack controller anymore

--- app/controllers/technology_stack_controller.rb
+++
@@ -1,9 +0,0 @@
-class TechnologyStackController < BaseController
-  
-  def index
-    screenshots_dir = File.join(Rails.root, "public", SETTINGS[:screenshots_dir], "/*")
-    @screenshots = Dir.glob(screenshots_dir).collect do |screenshot|
-      File.basename(screenshot)
-    end
-  end
-end
